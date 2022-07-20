6b41156240cce2efb6279c119c876d7c0345def6
HursitTarcan <hursittarcan@gmail.com>
2022-07-20 12:08:30 +0200

    Margin added under title [ABOUT]

--- src/components/About.js
+++ src/components/About.js
@@ -23,7 +23,7 @@ class About extends Component {
           </h1>
           <div className="row center mx-auto mb-5">
 
-            <div className="col-md-8 center" style={{ marginTop: '50px' }}>
+            <div className="col-md-8 center" style={{ marginTop: '20px' }}>
               <div className="col-md-10">
                 <div className="card">
                   <div className="card-header">
