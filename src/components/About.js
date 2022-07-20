54b182dee620e917203001eeda88ffa28f03096f
HursitTarcan <hursittarcan@gmail.com>
2022-07-20 10:54:50 +0200

    picture removed, about div centered. TODO: redesign to make better

--- src/components/About.js
+++ src/components/About.js
@@ -16,31 +16,12 @@ class About extends Component {
     }
 
     return (
-      <section id="about">
+      <section id="about" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: '0px'}}>
         <div className="col-md-12">
           <h1 style={{ color: "black" }}>
             <span>{sectionName}</span>
           </h1>
           <div className="row center mx-auto mb-5">
-            <div className="col-md-4 mb-5 center">
-              <div className="polaroid">
-                <span style={{ cursor: "auto" }}>
-                  <img
-                    height="260px"
-                    src={profilepic}
-                    alt="Avatar placeholder"
-                  />
-                  <Icon
-                    icon={reactIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
-                  />
-                  <Icon
-                    icon={vueIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
-                  />
-                </span>
-              </div>
-            </div>
 
             <div className="col-md-8 center">
               <div className="col-md-10">
