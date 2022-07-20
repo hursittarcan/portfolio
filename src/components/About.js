53756265bb800de1e83e78320707598b8302d1a8
HursitTarcan <hursittarcan@gmail.com>
2022-07-20 10:51:37 +0200

    picture added

--- src/components/About.js
+++ src/components/About.js
@@ -26,7 +26,7 @@ class About extends Component {
               <div className="polaroid">
                 <span style={{ cursor: "auto" }}>
                   <img
-                    height="250px"
+                    height="260px"
                     src={profilepic}
                     alt="Avatar placeholder"
                   />
