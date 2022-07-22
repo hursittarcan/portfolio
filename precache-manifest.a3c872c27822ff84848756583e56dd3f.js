741d944a6fc036c01aa33df6caefa7a72a25b8d3
HursitTarcan <hursittarcan@gmail.com>
2022-07-23 00:22:22 +0200

    Updates

---
+++ precache-manifest.a3c872c27822ff84848756583e56dd3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "931a7483e849224ca874dd0bc314b0c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bacdf8c417f0c794dfab",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "e61ca972e9415638cc94",
+    "url": "/static/css/main.12e6ab41.chunk.css"
+  },
+  {
+    "revision": "bacdf8c417f0c794dfab",
+    "url": "/static/js/2.26bdd67b.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.26bdd67b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e61ca972e9415638cc94",
+    "url": "/static/js/main.174626be.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
